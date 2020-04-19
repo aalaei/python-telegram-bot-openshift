99d3736b0d42c23ecaf3ceca7fa0ed5d8180d20f
aalaei <49282631+aalaei@users.noreply.github.com>
2020-04-19 21:21:15 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ def setup(webhook_url=None):
         updater = Updater(TOKEN)
         bot = updater.bot
         dp = updater.dispatcher
-    dp.add_handler(MessageHandler([], example_handler))  # Remove this line
+    dp.add_handler(MessageHandler(_UpdateType(), example_handler))  # Remove this line
     # Add your handlers here
     if webhook_url:
         bot.set_webhook(webhook_url=webhook_url)
